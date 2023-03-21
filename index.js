f82997916edc0a5cbad47c02018f0ead6806ad90
Renat Gofman <renatgofman@gmail.com>
2023-03-21 11:53:44 +0200

    Change id to catch latest block in nginx proxy

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-
-/**
- * Sovryn node health check
-*/
-const express= require('express');
+const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const Web3 = require('web3');
 const serverPort = 3001;
-let web3;
-let web3S;
-let web3Iov;
-// wait for rsk node to start
-setTimeout(startListening, 20000);
 
-function startListening(){
-   web3 = new Web3("http://127.0.0.1:4444");
-   web3S = new Web3("ws://127.0.0.1:4445/websocket");
-   web3Iov = new Web3('https://public-node.rsk.co');
+const web3 = new Web3("http://127.0.0.1:4444");
+const web3S = new Web3("ws://127.0.0.1:4445/websocket");
+const web3Iov = new Web3('https://public-node.rsk.co');
 
-   http.listen(serverPort, () => {
-       console.log('listening on *:'+serverPort);
-   });
+http.listen(serverPort, () => {
+  console.log('listening on *:' + serverPort);
+});
+
+async function getBlockNumber(web3) {
+  const result = await web3.eth.call({
+    method: 'eth_blockNumber',
+    params: [],
+    id: 777
+  });
+  return parseInt(result, 16);
 }
-app.get('/', async (req, res)=> {
-    const b = await web3.eth.getBlockNumber();
-    const s = await web3S.eth.getBlockNumber();
-    const i = await web3Iov.eth.getBlockNumber();
 
-    const result = new Date(Date.now())+ " processed blocks: rpc "+b+", wss: "+s+"  iov: "+i;
-    console.log(result);
+app.get('/', async (req, res) => {
+  const b = await getBlockNumber(web3);
+  const s = await getBlockNumber(web3S);
+  const i = await getBlockNumber(web3Iov);
 
-    if(Math.abs(b - s) <= 3 && Math.abs(b-i)<=3) return res.status(200).send(result);
+  const result = new Date(Date.now()) + " processed blocks: rpc " + b + ", wss: " + s + "  iov: " + i;
+  console.log(result);
 
+  if (Math.abs(b - s) <= 3 && Math.abs(b - i) <= 3) {
+    return res.status(200).send(result);
+  } else {
     return res.status(503).send("not in sync - " + result);
+  }
 });
