a15c6a853529192854f77d98dd699e083df54784
Renat Gofman <renatgofman@gmail.com>
2023-03-28 19:36:29 +0300

    fix rpc call syntax

--- index.js
+++ index.js
@@ -8,21 +8,41 @@ const http = require('http').createServer(app);
 const Web3 = require('web3');
 const serverPort = 3001;
 
-const web3 = new Web3("http://127.0.0.1:8080");
-const web3S = new Web3("ws://127.0.0.1:8080/websocket");
+const web3 = new Web3("http://3.138.67.27:4444");
+const web3S = new Web3("ws://3.138.67.27:4445/websocket");
 const web3Iov = new Web3('https://public-node.rsk.co');
 
 http.listen(serverPort, () => {
   console.log('listening on *:' + serverPort);
 });
 
+function sendAsyncRequest(web3, method, params) {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.send(
+            {
+                jsonrpc: "2.0",
+                id: 777,
+                method: method,
+                params: params
+            },
+            (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            }
+        );
+    });
+}
+
 async function getBlockNumber(web3) {
-  const result = await web3.eth.call({
-    method: 'eth_blockNumber',
-    params: [],
-    id: 777
-  });
-  return parseInt(result, 16);
+    try {
+        const result = (await sendAsyncRequest(web3, "eth_blockNumber", [])).result;
+        return parseInt(result, 16);
+    } catch (error) {
+        throw error;
+    }
 }
 
 app.get('/', async (req, res) => {
